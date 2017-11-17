a4d102802f5cf31a1e90bcc905b9946040ef4d5b
picheljitsu <pichelmayer@gmail.com>
2017-11-17 04:39:58 -0600

    Take an objdump output from an object file and output shellcode

---
+++ objd2shellcode.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys
+import os
+import re
+
+try:
+	objdump_file = sys.argv[1]
+	os.path.isfile(objdump_file)
+
+except:
+	print "Invalid Filename or File doesn't exist"
+	sys.exit(0)
+
+with open(objdump_file,'r') as f:
+	file_contents = f.readlines()
+
+hex_reg = re.compile(r'\s([a-f0-9]{2})')
+
+byte_string = ''
+
+for i in file_contents:
+	i = i.split('\t')
+	if len(i) == 3:
+		i = i[1].rstrip()
+		byte_string += i+' '
+
+format_to_bytearray = ''.join("\\x"+i for i in (byte_string.split()))
+
+print format_to_bytearray
+
